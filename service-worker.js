594d303d808af171d498880bd92699a49a453f1b
funortehub <ian.santos2639@soufunorte.com.br>
2025-07-12 02:44:22 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'axon-ai-cache-v1.0.1'; // Incremente a versão para forçar a atualização do cache
+const CACHE_NAME = 'axon-ai-cache-v1.0.2'; // Incremente a versão para forçar a atualização do cache
 const urlsToCache = [
   '/',
   '/index.html', // Ou o nome do seu arquivo HTML principal, se for diferente
